4040ab90e76ad4c328ba0499eb334e1a54d2e9f7
SaravanakumarS <83629985+sarwan1996@users.noreply.github.com>
2025-07-21 13:11:20 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import Card from "../components/Card";
+import Button from "../components/Button";
 
 const categories = [
   { name: "Men", image: "/images/men.jpg", path: "/men" },
